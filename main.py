99aec640fe5027766a0cd9b31789324cc5000159
Tim <4796173+Wizy@users.noreply.github.com>
2021-11-16 20:25:57 -0700

    Clear hands after game over

--- main.py
+++ main.py
@@ -57,24 +57,29 @@ def calc_hand(hand, is_dealer = False):
     return total
 
 def game_over():
-    global playing, round_over
+    global playing, round_over, players_hand, dealers_hand, bankroll, bet
     player_total = calc_hand(players_hand)
     dealer_total = calc_hand(dealers_hand)
     round_over = True
 
     if player_total == 21 and len(players_hand) == 2:
         print("You have blackjack, you win!")
+        bankroll += int(bet*1.5)
     elif dealer_total == 21 and len(dealers_hand) == 2:
         print("The dealer has blackjack, you lose!")
     elif player_total > 21:
         print("You busted. You lose!")
     elif dealer_total > 21:
         print("The dealer busted. You win!")
+        bankroll += bet
     elif player_total > dealer_total:
         print("You have "+str(player_total)+" while the dealer has "+str(dealer_total)+". You win!")
+        bankroll += bet
     elif dealer_total > player_total:
         print("You have "+str(player_total)+" while the dealer has "+str(dealer_total)+". You lose!")
-
+    
+    players_hand = []
+    dealers_hand = []
     response = input("Would you like to play again? Press any button to continue or 'n' to leave.")
     if response.lower() == 'n':
         playing = False
@@ -110,6 +115,7 @@ def draw(hand):
 print("Welcome to Blackjack!")
 while playing:
     bet = make_bet()
+    bankroll = bankroll - bet
     if len(shoe) < 20:
         shuffle_up()
         
@@ -125,5 +131,3 @@ while playing:
                 game_over()
 
 print("Goodbye.")
-shuffle_up()
-deal_out()
