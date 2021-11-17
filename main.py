1cfadf03a7907e0fa8f3d1d57b993c2061c29874
Tim <4796173+Wizy@users.noreply.github.com>
2021-11-16 21:38:41 -0700

    Lots of formatting. Mostly works

--- main.py
+++ main.py
@@ -6,14 +6,18 @@ bankroll = 500
 bet = 0
 shoe = []
 sorted_decks = []
-playing = True
+playing = False
 round_over = True
+spade = '\9824'
 for x in range(0,4): #Create sorted stack of 4 decks
-    for suit in ["Spades","Diamonds","Clubs","Hearts"]:
+    for suit in ["\u2660","\u2666","\u2663","\u2665"]:
         for face_value in range(2,12):
             sorted_decks.append({"suit":suit,"number":face_value})
         for face_value in range(0,3):
             sorted_decks.append({"suit":suit,"number":10})
+
+for card in sorted_decks:
+    print(card["suit"]+str(card["number"]))
 
 def shuffle_up():
     print("SHUFFLE UP!")
@@ -26,6 +30,14 @@ def shuffle_up():
     shoe = sorted_decks.copy()
     random.shuffle(shoe)
 
+def display_cards(hand):
+    output = ""
+    for card in hand:
+        temp = "{}{} "
+        output += temp.format(str(card["suit"]), str(card["number"]))
+    print(output)
+    print("Total: "+str(calc_hand(hand)))
+
 def deal_out():
     #simulate real deal out, I know it's unnecessary
     global shoe, round_over 
@@ -33,9 +45,13 @@ def deal_out():
     players_hand.append(shoe.pop())
     dealers_hand.append(shoe.pop())
     players_hand.append(shoe.pop())
-    print(players_hand)
-    print(dealers_hand)
-    print(calc_hand(players_hand))
+    #print(calc_hand(players_hand))
+    print("You have:")
+    display_cards(players_hand)
+    print("================================================================") #ugly divider :(
+    print("Dealer has:")
+    display_cards(dealers_hand)
+
     if calc_hand(players_hand) == 21:
         game_over()
 
@@ -64,69 +80,87 @@ def game_over():
 
     if player_total == 21 and len(players_hand) == 2:
         print("You have blackjack, you win!")
-        bankroll += int(bet*1.5)
+        bankroll += int(bet*1.5)+bet
     elif dealer_total == 21 and len(dealers_hand) == 2:
         print("The dealer has blackjack, you lose!")
     elif player_total > 21:
         print("You busted. You lose!")
     elif dealer_total > 21:
         print("The dealer busted. You win!")
-        bankroll += bet
+        bankroll += 2*bet
     elif player_total > dealer_total:
         print("You have "+str(player_total)+" while the dealer has "+str(dealer_total)+". You win!")
-        bankroll += bet
+        bankroll += 2*bet
     elif dealer_total > player_total:
         print("You have "+str(player_total)+" while the dealer has "+str(dealer_total)+". You lose!")
+    elif player_total == dealer_total:
+        print("You and the dealer have "+str(player_total)+". A tie! Your bet pushes.")
+        bankroll += bet
     
     players_hand = []
     dealers_hand = []
+    print("================================================================")
     response = input("Would you like to play again? Press any button to continue or 'n' to leave.")
     if response.lower() == 'n':
         playing = False
+    else:
+        make_bet()
 
 def make_bet():
+    global bankroll
     bet = 0
-    response = input("You have $"+str(bankroll)+", please place your bet or press 'n' to leave.")
-    if response.lower() == 'n':
-        playing = False
+    print("You have $"+str(bankroll)+".")
+
     while bet == 0:
+        response = input("Please place a bet or press 'n' to leave.\n")
+
         if response.lower() == 'n':
             playing = False
             return
-        try:
-            bet = int(response)
-            playing = True
-            return bet
-        except ValueError:
-            print("Please place a bet or press 'n' to leave.")
+        else:
+            bet = parse_bet(response)
+
+    bankroll = bankroll-bet
+    return bet
+    
+
+def parse_bet(user_input):
+    global playing
+    try:
+        value = int(user_input)
+        playing = True
+        print("yay")
+        return value
+    except ValueError:
+        print("boo")
+        return 0
 
 def draw(hand):
     global shoe
     card = shoe.pop()
     hand.append(card)
-    print("Your hand looks like this:")
-    print(hand)
-    print("Total: "+str(calc_hand(hand)))
-    if calc_hand(hand) >= 21:
-        game_over()
-
 
 #Main        
 print("Welcome to Blackjack!")
+bet = make_bet()
 while playing:
-    bet = make_bet()
-    bankroll = bankroll - bet
     if len(shoe) < 20:
         shuffle_up()
         
     deal_out()
     while not round_over:
-        response = input("Press any key to HIT, press 'n' to STAY.")
+        print("================================================================")
+        response = input("Press any key to HIT, press 'n' to STAY.\n")
         if response.lower() == 'n':
             while calc_hand(dealers_hand, True) < 17:
                 draw(dealers_hand)
+            print("Dealer has:")
+            display_cards(dealers_hand)
+            game_over()
         else:
             draw(players_hand)
+            print("You have:")
+            display_cards(players_hand)
             if calc_hand(players_hand) > 21:
                 game_over()
 
