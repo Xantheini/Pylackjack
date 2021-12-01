d354833d66c1631659ac93e6c56fba5060482f08
Xantheini <84294592+Xantheini@users.noreply.github.com>
2021-11-30 23:14:23 -0700

    Update main.py
    Added in lucky lucky. Still some bugs. Triple sevens won't work sometimes and suited comes up randomly, so will need to work that out.

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def display_cards(hand):
 
 def deal_out():
     #simulate real deal out, I know it's unnecessary
-    global shoe, round_over 
+    global shoe, round_over
     round_over = False
     players_hand.append(shoe.pop())
     dealers_hand.append(shoe.pop())
@@ -54,37 +54,122 @@ def deal_out():
     display_cards(dealers_hand)
 
     if calc_hand(players_hand) == 21:
-        game_over()
+        lucky_lucky() 
+        if calc_hand(dealers_hand, True) == 10 or calc_hand(dealers_hand, True) == 11:
+            draw(dealers_hand)
+            print("Dealer has:")
+            display_cards(dealers_hand)
+            game_over()
+        else: 
+            game_over()
+
+#Lucky Lucky Play
+def lucky_lucky(): 
+    global players_hand, dealers_hand, bankroll, lucky_bet
+
+    calc_both_hands = calc_hand(players_hand) + calc_hand(dealers_hand, True)
+    
+    #Aces 
+    if calc_both_hands > 21: 
+        if players_hand[0]["number"] == 11 or players_hand[1]["number"] == 11 or dealers_hand[0]["number"] == 11: 
+            calc_both_hands = calc_both_hands - 10
+    
+    #Suited Lucky
+    suited_lucky = False 
+    try: 
+        if players_hand[0]["suit"] == players_hand[1]["suit"] and players_hand[1]["suit"] == dealers_hand[0]["suit"]:
+            suited_lucky = True 
+    except IndexError: 
+        print(players_hand)
+        print(dealers_hand)
+    
+    #Lucky 678 
+    lucky_678 = False 
+    if calc_both_hands == 21: 
+        for card in players_hand: 
+            if card["number"] == 6: 
+                for card in players_hand: 
+                    if card["number"] == 7:
+                        lucky_678 = True 
+        for card in players_hand: 
+            if card["number"] == 7: 
+                for card in players_hand: 
+                    if card["number"] == 8:
+                        lucky_678 = True
+        for card in players_hand: 
+            if card["number"] == 8: 
+                for card in players_hand: 
+                    if card["number"] == 6:
+                        lucky_678 = True
+                        
+    #Lucky 777
+    lucky_777 = False
+    if players_hand[0]["number"] == 7 and players_hand[1]["number"] == 7 and dealers_hand[0]["number"] == 7: 
+        lucky_777 = True
+
+    if lucky_bet > 0: 
+        if calc_both_hands == 19 or calc_both_hands == 20:
+            bankroll += int(lucky_bet*2)+lucky_bet
+            print("Lucky Lucky " + str(calc_both_hands) + " pays 2 to 1.")
+        elif lucky_678 and suited_lucky: 
+            bankroll += int(lucky_bet*100)+lucky_bet
+            print("Lucky Lucky Suited 678 pays 100 to 1.")
+        elif lucky_678: 
+            bankroll += int(lucky_bet*30)+lucky_bet
+            print("Lucky Lucky 678 pays 30 to 1.")
+        elif lucky_777 and suited_lucky: 
+            bankroll += int(lucky_bet*200)+lucky_bet
+            print("Lucky Lucky Suited 777 pays 200 to 1.")
+        elif lucky_777: 
+            bankroll += int(lucky_bet*50)+lucky_bet
+            print("Lucky Lucky 777 pays 50 to 1.")
+        elif calc_both_hands == 21 and suited_lucky: 
+            bankroll += int(lucky_bet*15)+lucky_bet
+            print("Lucky Lucky Suited 21 pays 15 to 1.")
+        elif calc_both_hands == 21: 
+            bankroll += int(lucky_bet*3)+lucky_bet
+            print("Lucky Lucky 21 pays 3 to 1.")
+        else:
+            print("No Lucky Lucky.")
+
+    else: 
+      return
 
 def calc_hand(hand, is_dealer = False):
     total = 0
     index_of_aces = []
     for card in hand:
-        card_value = card["number"]
+        card_value = card.get("number")
         total+=card_value
         if card_value == 11:
             index_of_aces.append(hand.index(card))
-
-    if is_dealer and total < 18 and len(index_of_aces) > 0: #Dealer hits on soft 17 and less, so calculate as lesser value
-        hand[index_of_aces.pop()] = 1
+  
+    if is_dealer and total > 11 and total < 18 and len(index_of_aces) > 0: #Dealer hits on soft 17 and less, so calculate as lesser value
+        hand[index_of_aces.pop()]["number"] = 1
         total = total - 10
     elif total > 21 and len(index_of_aces) > 0: #Player can hit on any soft value
-        hand[index_of_aces.pop()] = 1
+        hand[index_of_aces.pop()]["number"] = 1
         total = total - 10
 
     return total
 
 def game_over():
-    global playing, round_over, players_hand, dealers_hand, bankroll, bet
+    global playing, round_over, players_hand, dealers_hand, bankroll, bet,lucky_bet
     player_total = calc_hand(players_hand)
     dealer_total = calc_hand(dealers_hand)
     round_over = True
 
-    if player_total == 21 and len(players_hand) == 2:
+    if player_total == 21 and len(players_hand) == 2 and dealer_total == 21 and len(dealers_hand) == 2:
+        print("You and the dealer have blackjack! Your bet pushes.")
+        bankroll += bet
+    elif player_total == 21 and len(players_hand) == 2:
         print("You have blackjack, you win!")
         bankroll += int(bet*1.5)+bet
     elif dealer_total == 21 and len(dealers_hand) == 2:
         print("The dealer has blackjack, you lose!")
+    elif player_total == dealer_total:
+        print("You and the dealer have "+str(player_total)+". A tie! Your bet pushes.")
+        bankroll += bet 
     elif player_total > 21:
         print("You busted. You lose!")
     elif dealer_total > 21:
@@ -95,21 +180,26 @@ def game_over():
         bankroll += 2*bet
     elif dealer_total > player_total:
         print("You have "+str(player_total)+" while the dealer has "+str(dealer_total)+". You lose!")
-    elif player_total == dealer_total:
-        print("You and the dealer have "+str(player_total)+". A tie! Your bet pushes.")
-        bankroll += bet
     
     players_hand = []
     dealers_hand = []
     print("================================================================")
+    if bankroll == 0: 
+        answer = input("You've run out of money! Type 'y' if you'd like to buy in for another $500. ")
+        if answer.lower() == 'y': 
+            bankroll += 500 
+        else:
+            playing = False
+            return
     response = input("Would you like to play again? Press any button to continue or 'n' to leave.")
     if response.lower() == 'n':
         playing = False
     else:
         bet = make_bet()
+        lucky_bet = make_lucky_bet()
 
 def make_bet():
-    global bankroll
+    global bankroll, playing
     bet = 0
     print("You have $"+str(bankroll)+".")
 
@@ -121,11 +211,33 @@ def make_bet():
             return
         else:
             bet = parse_bet(response)
+    if bet > bankroll: 
+        print("You don't have enough. ")
+        response = input("Please place a bet or press 'n' to leave.\n")
 
+        if response.lower() == 'n':
+            playing = False
+            return
+        else:
+            bet = parse_bet(response)
     bankroll = bankroll-bet
     return bet
-    
 
+def make_lucky_bet():
+    #Lucky Lucky Bet
+    global bankroll
+    lucky_bet = 0 
+    print("You have $"+str(bankroll)+".")
+    while lucky_bet == 0: 
+        lucky_response = input("Please place a bet for Lucky Lucky or press 'n' to continue. \n")
+        if lucky_response.lower() == 'n': 
+            break
+        else: 
+            lucky_bet = int(lucky_response)
+    bankroll = bankroll-lucky_bet
+    return lucky_bet
+
+    
 def parse_bet(user_input):
     global playing
     try:
@@ -143,11 +255,13 @@ def draw(hand):
 #Main        
 print("Welcome to Blackjack!")
 bet = make_bet()
+lucky_bet = make_lucky_bet()
 while playing:
     if len(shoe) < 20:
         shuffle_up()
         
     deal_out()
+    lucky_lucky()
     while not round_over:
         print("================================================================")
         
@@ -158,6 +272,7 @@ while playing:
             print("Dealer has:")
             display_cards(dealers_hand)
             game_over()
+            break
 
         response = input("Press any key to HIT, press 'n' to STAY.\n")
         if response.lower() == 'n':
@@ -174,4 +289,4 @@ while playing:
             if calc_hand(players_hand) > 21:
                 game_over()
 
-print("Goodbye.")
+print("Goodbye.") 
